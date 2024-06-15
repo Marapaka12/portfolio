20c6ae4c5242362b7fc1fc3baaff120d429624d1
Marapaka12 <164732733+Marapaka12@users.noreply.github.com>
2024-06-15 14:52:32 +0530

    set homepage for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Rani Marapaka",
+  title: "Full Stack Java Developer",
+  email: "marapakarani12@gmail.com",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "Marapaka Rani",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
